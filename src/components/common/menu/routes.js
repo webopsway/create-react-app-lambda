95f58c5d41287ef138296103b769f21d5893de77
webopsway <webmaster@opsway.fr>
2024-08-03 23:37:54 +0200

    Update Nabar

--- src/components/common/menu/routes.js
+++ src/components/common/menu/routes.js
@@ -1,12 +1,12 @@
 export const routes = [
 	{
 		title: "Opsway",
-		href: "#",
-		subRoutes: [
-			{
-				title: "Design Agency",
-				href: "",
-			},
+		href: "home-one",
+		//subRoutes: [
+		//	{
+		//		title: "Design Agency",
+		//		href: "",
+		//	},
 			//{
 			//	title: "Startup Agency",
 			//	href: "home-two",
@@ -40,120 +40,120 @@ export const routes = [
 	{
 		title: "Service",
 		href: "service",
-		subRoutes: [
-			{
-				title: "About Us",
-				href: "about-us",
-			},
-			{
-				title: "Pricing",
-				href: "pricing",
-			},
-			{
-				title: "Blog",
-				href: "#",
-				subRoutes: [
-					{
-						title: "Our Blog",
-						href: "blog",
-					},
-					{
-						title: "Blog Grid",
-						href: "blog-grid",
-					},
-					{
-						title: "Blog Details",
-						href: "single-blog",
-					},
-				],
-			},
-			{
-				title: "Service",
-				href: "#",
-				subRoutes: [
-					{
-						title: "Service",
-						href: "service",
-					},
-					{
-						title: "Service Details",
-						href: "single-service",
-					},
-				],
-			},
-			{
-				title: "Team",
-				href: "#",
-				subRoutes: [
-					{
-						title: "Team",
-						href: "team",
-					},
-					{
-						title: "Team Details",
-						href: "single-team",
-					},
-				],
-			},
-			{
-				title: "Portfolio",
-				href: "#",
-				subRoutes: [
-					{
-						title: "Portfolio One Column",
-						href: "portfolio-one",
-					},
-					{
-						title: "Portfolio Two Column",
-						href: "portfolio-two",
-					},
-					{
-						title: "Single Portfolio",
-						href: "single-portfolio",
-					},
-				],
-			},
-			{
-				title: "Utility",
-				href: "#",
-				subRoutes: [
-					{
-						title: "Faq",
-						href: "faq",
-					},
-					{
-						title: "Error 404",
-						href: "error-page",
-					},
-					{
-						title: "Testimonial",
-						href: "testimonial",
-					},
-					{
-						title: "Coming Soon",
-						href: "coming-soon",
-					},
-				],
-			},
-			{
-				title: "Account",
-				href: "#",
-				subRoutes: [
-					{
-						title: "Sign Up",
-						href: "sign-up",
-					},
-					{
-						title: "Sign In",
-						href: "sign-in",
-					},
-					{
-						title: "Reset Password",
-						href: "reset-password",
-					},
-				],
-			},
-		],
+	//	subRoutes: [
+	//		{
+	//			title: "About Us",
+	//			href: "about-us",
+	//		},
+	//		{
+	//			title: "Pricing",
+	//			href: "pricing",
+	//		},
+	//		{
+	//			title: "Blog",
+	//			href: "#",
+	//			subRoutes: [
+	//				{
+	//					title: "Our Blog",
+	//					href: "blog",
+	//				},
+	//				{
+	//					title: "Blog Grid",
+	//					href: "blog-grid",
+	//				},
+	//				{
+	//					title: "Blog Details",
+	//					href: "single-blog",
+	//				},
+	//			],
+	//		},
+	//		{
+	//			title: "Service",
+	//			href: "#",
+	//			subRoutes: [
+	//				{
+	//					title: "Service",
+	//					href: "service",
+	//				},
+	//				{
+	//					title: "Service Details",
+	//					href: "single-service",
+	//				},
+	//			],
+	//		},
+	//		{
+	//			title: "Team",
+	//			href: "#",
+	//			subRoutes: [
+	//				{
+	//					title: "Team",
+	//					href: "team",
+	//				},
+	//				{
+	//					title: "Team Details",
+	//					href: "single-team",
+	//				},
+	//			],
+	//		},
+	//		{
+	//			title: "Portfolio",
+	//			href: "#",
+	//			subRoutes: [
+	//				{
+	//					title: "Portfolio One Column",
+	//					href: "portfolio-one",
+	//				},
+	//				{
+	//					title: "Portfolio Two Column",
+	//					href: "portfolio-two",
+	//				},
+	//				{
+	//					title: "Single Portfolio",
+	//					href: "single-portfolio",
+	//				},
+	//			],
+	//		},
+	//		{
+	//			title: "Utility",
+	//			href: "#",
+	//			subRoutes: [
+	//				{
+	//					title: "Faq",
+	//					href: "faq",
+	//				},
+	//				{
+	//					title: "Error 404",
+	//					href: "error-page",
+	//				},
+	//				{
+	//					title: "Testimonial",
+	//					href: "testimonial",
+	//				},
+	//				{
+	//					title: "Coming Soon",
+	//					href: "coming-soon",
+	//				},
+	//			],
+	//		},
+	//		{
+	//			title: "Account",
+	//			href: "#",
+	//			subRoutes: [
+	//				{
+	//					title: "Sign Up",
+	//					href: "sign-up",
+	//				},
+	//				{
+	//					title: "Sign In",
+	//					href: "sign-in",
+	//				},
+	//				{
+	//					title: "Reset Password",
+	//					href: "reset-password",
+	//				},
+	//			],
+	//		},
+	//	],
 	},
 	{
 		title: "Blog",
